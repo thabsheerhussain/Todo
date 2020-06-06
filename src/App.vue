73cf608a4c9c99e5440e0cbc976b0ab496bf2e53
TABY-8 <44165496+thabsheerhussain@users.noreply.github.com>
2020-06-06 13:01:22 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addtodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    AddTodo,    
+    Todos
+
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id:1,
+          title: "Todo One",
+          completed:false 
+        },
+        {
+          id:2,
+          title: "Todo Two",
+          completed:true 
+        },
+        {
+          id:3,
+          title: "Todo Three",
+          completed:false 
+        },
+        {
+          id:4,
+          title: "Todo Four",
+          completed:false 
+        },
+      ]
+    }
+    },
+    methods: {
+      deleteTodo(id){
+        this.todos = this.todos.filter(todo => todo.id !== id);
+        
+      },
+      addtodo(newTodo){
+        this.todos = [...this.todos, newTodo];
+        this.title = 'n';
+      }
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
